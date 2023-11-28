98b5ec3ad14c7432cac11bf6dccfc260d3ae3242
Dario578 <a22310218@ceti.mx>
2023-11-27 18:19:09 -0600

    Revert "tratando balas 2"
    This reverts commit 069cb47a389c6eabb2122680762d0ffcc5992803.

--- include/Ship.hpp
+++ include/Ship.hpp
@@ -1,66 +1,50 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "Game.hpp" // Assuming Game.hpp contains necessary declarations
-#include "Bullet.hpp" // Assuming you have a Bullet class
+#include <Game.hpp>
 
 class Ship : public sf::Sprite
 {
 protected:
     sf::IntRect _sprite;
+    // Default constructor is hidden
+    Ship() {}
 
 public:
-    Ship(sf::IntRect ir) : sf::Sprite(), _sprite(ir)
+    // Constructor that takes a sprite
+    Ship(sf::IntRect ir) : Sprite()
     {
+        _sprite = ir;
         setTexture(spritesheet);
         setTextureRect(_sprite);
-    }
-
+    };
+    // Pure virtual deconstructor -- makes this an abstract class
     virtual ~Ship() = default;
-
-    virtual void Update(const float &dt) {}
+    // Update, virtual so can be overrided, but not pure virtual
+    virtual void Update(const float &dt){}
 };
 
 class Player : public Ship {
-private:
-    static std::vector<Bullet*> bullets;
-
 public:
     Player() : Ship(sf::IntRect(135, 0, 40, 32))
     {
-        setPosition({gameHeight * 0.5f, gameHeight - 32.f});
+        setPosition({gameHeight * .5f, gameHeight - 32.f});
     }
 
     void Update(const float &dt) override {
         Ship::Update(dt);
-
-        // Fire bullet
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-            bullets.push_back(new Bullet(getPosition(), false));
+        // Mueve a la izquierda
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            move(-80.0f * dt, 0.0f);
+        }
+        // Mueve a la derecha
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            move(80.0f * dt, 0.0f);
         }
 
-        // Update bullets
-        for (auto& bullet : bullets) {
-            bullet->Update(dt);
+        // Limita la posición vertical para que no baje con el desplazamiento de la ventana
+        sf::Vector2f position = getPosition();
+        if (position.y > gameHeight - 32.f) {
+            setPosition(position.x, gameHeight - 32.f);
         }
-
-        // Remove bullets that are off-screen or have hit something (add your logic)
-        bullets.erase(
-            std::remove_if(
-                bullets.begin(),
-                bullets.end(),
-                [](Bullet* bullet) {
-                    // Add your condition for removing bullets, e.g., off-screen check
-                    return bullet->getPosition().y < 0; // Remove when bullet goes off the top of the screen
-                }
-            ),
-            bullets.end()
-        );
-    }
-
-    static std::vector<Bullet*>& GetBullets() {
-        return bullets;
     }
 };
-
-// Initialize the static vector
-std::vector<Bullet*> Player::bullets;
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,42 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
 #include <iostream>
-#include <vector>
-#include "Ship.hpp"
-#include "Game.hpp"
-#include "Invader.hpp"
-#include "Bullet.hpp" // Assuming you have a Bullet class
+#include <Ship.hpp>
+#include <Game.hpp>
+#include <Invader.hpp>
 
 sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
 sf::Texture spritesheet;
-sf::Texture backgroundTexture;
-std::vector<Ship*> ships; // Assuming you have a vector to store ships
+sf::Sprite invader;
 
 bool Invader::direction = true;
 float Invader::speed = 5.0f;
 
+// Define the ships vector for Invader
 void Load() {
-    // Loading textures, initializing ships, and adding them to the vector
     if (!spritesheet.loadFromFile("assets/images/SpritesheetSW2.png")) {
         std::cerr << "Failed to load spritesheet!" << std::endl;
     }
-    if (!backgroundTexture.loadFromFile("assets/images/Background.jpg")) {
-        std::cerr << "Failed to load background image!" << std::endl;
-    }
 
+    // Generate a grid of invaders
     for (int r = 0; r < invaders_rows; ++r) {
-        sf::IntRect rect(0, 0, 38, 32);
+        sf::IntRect rect(0, 0, 38, 32); // Adjust the values accordingly
         for (int c = 0; c < invaders_columns; ++c) {
-            sf::Vector2f position(100.0f + c * 40.0f, 100.0f + r * 40.0f);
+            sf::Vector2f position(100.0f + c * 40.0f, 100.0f + r * 40.0f); // Adjust the values accordingly
             Invader* inv = new Invader(rect, position);
             ships.push_back(inv);
         }
     }
-
     Player* player = new Player();
     ships.push_back(player);
 }
 
 void Render() {
-    // Drawing ships
     for (const auto& s : ships) {
         window.draw(*s);
     }
 }
 
 void Update(float dt) {
-    // Updating ships
     for (auto& s : ships) {
         s->Update(dt);
     }
@@ -66,16 +56,8 @@ int main() {
         }
 
         float dt = clock.restart().asSeconds();
-
-        // Handle player input and update
-        ships.back()->Update(dt);
-
-        // Fire bullet when space key is pressed
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-            ships.back()->FireBullet();
-        }
-
-        // Update all ships, including bullets
+        
+        // Actualiza todas las naves (incluida la del jugador)
         Update(dt);
 
         window.clear();
