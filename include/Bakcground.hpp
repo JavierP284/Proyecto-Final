35c67c4e3f454314464a3279ed859447bd477196
JavierP284 <a22310284@ceti.mx>
2023-11-28 23:57:22 -0600

    puml

---
+++ include/Bakcground.hpp
@@ -0,0 +1,16 @@
+class Bakcground
+{
+private:
+    /* data */
+public:
+    Bakcground(/* args */);
+    ~Bakcground();
+};
+
+Bakcground::Bakcground(/* args */)
+{
+}
+
+Bakcground::~Bakcground()
+{
+}
