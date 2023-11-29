6ef03d6f91ff8ad7259cf06cca18fb3e9f7684e1
JavierP284 <a22310284@ceti.mx>
2023-11-28 23:23:28 -0600

    Disparo con cooldown

--- include/Ship.hpp
+++ include/Ship.hpp
@@ -24,28 +24,38 @@ public:
 };
 
 class Player : public Ship {
+private:
+    float shootCooldown;  // Tiempo de enfriamiento entre disparos en segundos
+    float timeSinceLastShoot;  // Tiempo transcurrido desde el último disparo
+
 public:
-    Player() : Ship(sf::IntRect(135, 0, 40, 40))
-    {
-        setPosition({gameHeight * .5f, gameHeight - 32.f});
+    Player() : Ship(sf::IntRect(135, 0, 40, 40)), shootCooldown(0.5f), timeSinceLastShoot(0.0f) {
+        setPosition({gameWidth * 0.5f, gameHeight - 32.0f});
     }
 
     void Update(const float &dt) override {
         Ship::Update(dt);
 
+        // Actualiza el tiempo transcurrido desde el último disparo
+        timeSinceLastShoot += dt;
+
         // Mueve a la izquierda
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
             move(-80.0f * dt, 0.0f);
         }
+
         // Mueve a la derecha
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
             move(80.0f * dt, 0.0f);
         }
 
-        // Dispara balas con la barra espaciadora
-         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-        Bullet* bullet = new Bullet(getPosition() + sf::Vector2f(0, -16), spritesheet, false);
-        bullets.push_back(bullet);
+        // Dispara balas con la barra espaciadora si ha pasado el tiempo de enfriamiento
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && timeSinceLastShoot >= shootCooldown) {
+            Bullet* bullet = new Bullet(getPosition() + sf::Vector2f(0, -16), spritesheet, false);
+            bullets.push_back(bullet);
+
+            // Reinicia el tiempo desde el último disparo
+            timeSinceLastShoot = 0.0f;
         }
 
         // Limita la posición vertical para que no baje con el desplazamiento de la ventana
